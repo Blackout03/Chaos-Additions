e0af7afabe8155ae4aa06a983418e8de8fdabb31
Blackout03 <gardner.hayden03@gmail.com>
2021-08-09 23:27:29 +1000

    AIOT Changes

--- src/main/java/com/blackout/caadditions/items/AIOTItem.java
+++ src/main/java/com/blackout/caadditions/items/AIOTItem.java
@@ -1,7 +1,9 @@
 package com.blackout.caadditions.items;
 
 import com.blackout.caadditions.util.ToolUtil;
+import io.github.chaosawakens.common.registry.CABlocks;
 import net.minecraft.block.*;
+import net.minecraft.block.material.Material;
 import net.minecraft.client.gui.screen.Screen;
 import net.minecraft.client.util.ITooltipFlag;
 import net.minecraft.entity.player.PlayerEntity;
@@ -29,7 +31,9 @@ public class AIOTItem extends ToolItem {
     }
 
     public float getDestroySpeed(ItemStack itemStack, BlockState blockState) {
-        return super.getDestroySpeed(itemStack, blockState);
+        Material material = blockState.getMaterial();
+        Block block = blockState.getBlock();
+        return block != CABlocks.TERMITE_INFESTED_ORE.get() && block != CABlocks.RED_ANT_INFESTED_ORE.get() && block != CABlocks.FOSSILISED_EMERALD_GATOR.get() && block != CABlocks.FOSSILISED_ENT.get() && block != CABlocks.FOSSILISED_HERCULES_BEETLE.get() && block != CABlocks.FOSSILISED_RUBY_BUG.get() && block != Blocks.NETHER_WART_BLOCK && block != Blocks.WARPED_WART_BLOCK && block != Blocks.HAY_BLOCK && block != Blocks.DRIED_KELP_BLOCK && block != Blocks.TARGET && block != Blocks.SHROOMLIGHT && block != Blocks.SPONGE && block != Blocks.WET_SPONGE && block != Blocks.JUNGLE_LEAVES && block != Blocks.OAK_LEAVES && block != Blocks.SPRUCE_LEAVES && block != Blocks.DARK_OAK_LEAVES && block != Blocks.ACACIA_LEAVES && block != Blocks.BIRCH_LEAVES && block != Blocks.CLAY && block != Blocks.DIRT && block != Blocks.COARSE_DIRT && block != Blocks.PODZOL && block != Blocks.FARMLAND && block != Blocks.GRASS_BLOCK && block != Blocks.GRAVEL && block != Blocks.MYCELIUM && block != Blocks.SAND && block != Blocks.RED_SAND && block != Blocks.SNOW_BLOCK && block != Blocks.SNOW && block != Blocks.SOUL_SAND && block != Blocks.GRASS_PATH && block != Blocks.WHITE_CONCRETE_POWDER && block != Blocks.ORANGE_CONCRETE_POWDER && block != Blocks.MAGENTA_CONCRETE_POWDER && block != Blocks.LIGHT_BLUE_CONCRETE_POWDER && block != Blocks.YELLOW_CONCRETE_POWDER && block != Blocks.LIME_CONCRETE_POWDER && block != Blocks.PINK_CONCRETE_POWDER && block != Blocks.GRAY_CONCRETE_POWDER && block != Blocks.LIGHT_GRAY_CONCRETE_POWDER && block != Blocks.CYAN_CONCRETE_POWDER && block != Blocks.PURPLE_CONCRETE_POWDER && block != Blocks.BLUE_CONCRETE_POWDER && block != Blocks.BROWN_CONCRETE_POWDER && block != Blocks.GREEN_CONCRETE_POWDER && block != Blocks.RED_CONCRETE_POWDER && block != Blocks.BLACK_CONCRETE_POWDER && block != Blocks.SOUL_SOIL && material != Material.WOOD && material != Material.NETHER_WOOD && material != Material.PLANT && material != Material.REPLACEABLE_PLANT && material != Material.BAMBOO && material != Material.VEGETABLE && material != Material.METAL && material != Material.HEAVY_METAL && material != Material.STONE ? super.getDestroySpeed(itemStack, blockState) : this.speed;
     }
 
     public ActionResultType useOn(ItemUseContext itemUseContext) {
--- src/main/java/com/blackout/caadditions/registry/CAAdditionsItems.java
+++ src/main/java/com/blackout/caadditions/registry/CAAdditionsItems.java
@@ -2,9 +2,9 @@ package com.blackout.caadditions.registry;
 
 import com.blackout.caadditions.CAAdditions;
 import com.blackout.caadditions.config.CAAdditionsConfig;
+import com.blackout.caadditions.enums.CAAdditionsItemTier;
 import com.blackout.caadditions.items.AIOTItem;
 import com.blackout.caadditions.items.EnchantedAIOTItem;
-import io.github.chaosawakens.common.enums.ToolMaterials;
 import io.github.chaosawakens.common.registry.CAItemGroups;
 import net.minecraft.enchantment.EnchantmentData;
 import net.minecraft.enchantment.Enchantments;
@@ -26,19 +26,19 @@ public class CAAdditionsItems {
     public static final RegistryObject<AIOTItem> DIAMOND_AIOT = ITEMS.register("diamond_aiot", () -> new AIOTItem(ItemTier.DIAMOND, CAAdditionsConfig.COMMON.diamondAIOTDamage.get() - 4, -2.4F, new Item.Properties().tab(CAItemGroups.equipmentItemGroup)));
     public static final RegistryObject<AIOTItem> NETHERITE_AIOT = ITEMS.register("netherite_aiot", () -> new AIOTItem(ItemTier.NETHERITE, CAAdditionsConfig.COMMON.netheriteAIOTDamage.get() - 5, -2.4F, new Item.Properties().tab(CAItemGroups.equipmentItemGroup)));
 
-    public static final RegistryObject<AIOTItem> CRYSTAL_WOOD_AIOT = ITEMS.register("crystal_wood_aiot", () -> new AIOTItem(ToolMaterials.TOOL_CRYSTAL_WOOD, CAAdditionsConfig.COMMON.crystalWoodAIOTDamage.get() - 2, -2.4F, new Item.Properties().tab(CAItemGroups.equipmentItemGroup)));
-    public static final RegistryObject<AIOTItem> KYANITE_AIOT = ITEMS.register("kyanite_aiot", () -> new AIOTItem(ToolMaterials.TOOL_KYANITE, CAAdditionsConfig.COMMON.kyaniteAIOTDamage.get() - 3, -2.4F, new Item.Properties().tab(CAItemGroups.equipmentItemGroup)));
-    public static final RegistryObject<AIOTItem> PINK_TOURMALINE_AIOT = ITEMS.register("pink_tourmaline_aiot", () -> new AIOTItem(ToolMaterials.TOOL_PINK_TOURMALINE, CAAdditionsConfig.COMMON.pinkTourmalineAIOTDamage.get() - 9, -2.4F, new Item.Properties().tab(CAItemGroups.equipmentItemGroup)));
-    public static final RegistryObject<AIOTItem> CATS_EYE_AIOT = ITEMS.register("cats_eye_aiot", () -> new AIOTItem(ToolMaterials.TOOL_CATS_EYE, CAAdditionsConfig.COMMON.catsEyeAIOTDamage.get() - 9, -2.4F, new Item.Properties().tab(CAItemGroups.equipmentItemGroup)));
-    public static final RegistryObject<AIOTItem> COPPER_AIOT = ITEMS.register("copper_aiot", () -> new AIOTItem(ToolMaterials.TOOL_COPPER, CAAdditionsConfig.COMMON.copperAIOTDamage.get() - 3, -2.4F, new Item.Properties().tab(CAItemGroups.equipmentItemGroup)));
-    public static final RegistryObject<AIOTItem> TIN_AIOT = ITEMS.register("tin_aiot", () -> new AIOTItem(ToolMaterials.TOOL_TIN, CAAdditionsConfig.COMMON.tinAIOTDamage.get() - 4, -2.4F, new Item.Properties().tab(CAItemGroups.equipmentItemGroup)));
-    public static final RegistryObject<AIOTItem> SILVER_AIOT = ITEMS.register("silver_aiot", () -> new AIOTItem(ToolMaterials.TOOL_SILVER, CAAdditionsConfig.COMMON.silverAIOTDamage.get() - 5, -2.4F, new Item.Properties().tab(CAItemGroups.equipmentItemGroup)));
-    public static final RegistryObject<AIOTItem> PLATINUM_AIOT = ITEMS.register("platinum_aiot", () -> new AIOTItem(ToolMaterials.TOOL_PLATINUM, CAAdditionsConfig.COMMON.platinumAIOTDamage.get() - 7, -2.4F, new Item.Properties().tab(CAItemGroups.equipmentItemGroup)));
-    public static final RegistryObject<AIOTItem> EMERALD_AIOT = ITEMS.register("emerald_aiot", () -> new EnchantedAIOTItem(ToolMaterials.TOOL_EMERALD, CAAdditionsConfig.COMMON.emeraldAIOTDamage.get() - 7, -2.4F, new Item.Properties().tab(CAItemGroups.equipmentItemGroup), new EnchantmentData[] { new EnchantmentData(Enchantments.SILK_TOUCH, 1)}));
-    public static final RegistryObject<AIOTItem> AMETHYST_AIOT = ITEMS.register("amethyst_aiot", () -> new AIOTItem(ToolMaterials.TOOL_AMETHYST, CAAdditionsConfig.COMMON.amethystAIOTDamage.get() - 12, -2.4F, new Item.Properties().tab(CAItemGroups.equipmentItemGroup)));
-    public static final RegistryObject<AIOTItem> TIGERS_EYE_AIOT = ITEMS.register("tigers_eye_aiot", () -> new AIOTItem(ToolMaterials.TOOL_TIGERS_EYE, CAAdditionsConfig.COMMON.tigersEyeAIOTDamage.get() - 9, -2.4F, new Item.Properties().tab(CAItemGroups.equipmentItemGroup)));
-    public static final RegistryObject<AIOTItem> RUBY_AIOT = ITEMS.register("ruby_aiot", () -> new AIOTItem(ToolMaterials.TOOL_RUBY, CAAdditionsConfig.COMMON.rubyAIOTDamage.get() - 17, -2.4F, new Item.Properties().tab(CAItemGroups.equipmentItemGroup)));
-    public static final RegistryObject<AIOTItem> ULTIMATE_AIOT = ITEMS.register("ultimate_aiot", () -> new EnchantedAIOTItem(ToolMaterials.TOOL_ULTIMATE, CAAdditionsConfig.COMMON.ultimateAIOTDamage.get() - 37, -2.4F, new Item.Properties().tab(CAItemGroups.equipmentItemGroup),
+    public static final RegistryObject<AIOTItem> CRYSTAL_WOOD_AIOT = ITEMS.register("crystal_wood_aiot", () -> new AIOTItem(CAAdditionsItemTier.TOOL_CRYSTAL_WOOD, CAAdditionsConfig.COMMON.crystalWoodAIOTDamage.get() - 2, -2.4F, new Item.Properties().tab(CAItemGroups.equipmentItemGroup)));
+    public static final RegistryObject<AIOTItem> KYANITE_AIOT = ITEMS.register("kyanite_aiot", () -> new AIOTItem(CAAdditionsItemTier.TOOL_KYANITE, CAAdditionsConfig.COMMON.kyaniteAIOTDamage.get() - 3, -2.4F, new Item.Properties().tab(CAItemGroups.equipmentItemGroup)));
+    public static final RegistryObject<AIOTItem> PINK_TOURMALINE_AIOT = ITEMS.register("pink_tourmaline_aiot", () -> new AIOTItem(CAAdditionsItemTier.TOOL_PINK_TOURMALINE, CAAdditionsConfig.COMMON.pinkTourmalineAIOTDamage.get() - 9, -2.4F, new Item.Properties().tab(CAItemGroups.equipmentItemGroup)));
+    public static final RegistryObject<AIOTItem> CATS_EYE_AIOT = ITEMS.register("cats_eye_aiot", () -> new AIOTItem(CAAdditionsItemTier.TOOL_CATS_EYE, CAAdditionsConfig.COMMON.catsEyeAIOTDamage.get() - 9, -2.4F, new Item.Properties().tab(CAItemGroups.equipmentItemGroup)));
+    public static final RegistryObject<AIOTItem> COPPER_AIOT = ITEMS.register("copper_aiot", () -> new AIOTItem(CAAdditionsItemTier.TOOL_COPPER, CAAdditionsConfig.COMMON.copperAIOTDamage.get() - 3, -2.4F, new Item.Properties().tab(CAItemGroups.equipmentItemGroup)));
+    public static final RegistryObject<AIOTItem> TIN_AIOT = ITEMS.register("tin_aiot", () -> new AIOTItem(CAAdditionsItemTier.TOOL_TIN, CAAdditionsConfig.COMMON.tinAIOTDamage.get() - 4, -2.4F, new Item.Properties().tab(CAItemGroups.equipmentItemGroup)));
+    public static final RegistryObject<AIOTItem> SILVER_AIOT = ITEMS.register("silver_aiot", () -> new AIOTItem(CAAdditionsItemTier.TOOL_SILVER, CAAdditionsConfig.COMMON.silverAIOTDamage.get() - 5, -2.4F, new Item.Properties().tab(CAItemGroups.equipmentItemGroup)));
+    public static final RegistryObject<AIOTItem> PLATINUM_AIOT = ITEMS.register("platinum_aiot", () -> new AIOTItem(CAAdditionsItemTier.TOOL_PLATINUM, CAAdditionsConfig.COMMON.platinumAIOTDamage.get() - 7, -2.4F, new Item.Properties().tab(CAItemGroups.equipmentItemGroup)));
+    public static final RegistryObject<AIOTItem> EMERALD_AIOT = ITEMS.register("emerald_aiot", () -> new EnchantedAIOTItem(CAAdditionsItemTier.TOOL_EMERALD, CAAdditionsConfig.COMMON.emeraldAIOTDamage.get() - 7, -2.4F, new Item.Properties().tab(CAItemGroups.equipmentItemGroup), new EnchantmentData[] { new EnchantmentData(Enchantments.SILK_TOUCH, 1)}));
+    public static final RegistryObject<AIOTItem> AMETHYST_AIOT = ITEMS.register("amethyst_aiot", () -> new AIOTItem(CAAdditionsItemTier.TOOL_AMETHYST, CAAdditionsConfig.COMMON.amethystAIOTDamage.get() - 12, -2.4F, new Item.Properties().tab(CAItemGroups.equipmentItemGroup)));
+    public static final RegistryObject<AIOTItem> TIGERS_EYE_AIOT = ITEMS.register("tigers_eye_aiot", () -> new AIOTItem(CAAdditionsItemTier.TOOL_TIGERS_EYE, CAAdditionsConfig.COMMON.tigersEyeAIOTDamage.get() - 9, -2.4F, new Item.Properties().tab(CAItemGroups.equipmentItemGroup)));
+    public static final RegistryObject<AIOTItem> RUBY_AIOT = ITEMS.register("ruby_aiot", () -> new AIOTItem(CAAdditionsItemTier.TOOL_RUBY, CAAdditionsConfig.COMMON.rubyAIOTDamage.get() - 17, -2.4F, new Item.Properties().tab(CAItemGroups.equipmentItemGroup)));
+    public static final RegistryObject<AIOTItem> ULTIMATE_AIOT = ITEMS.register("ultimate_aiot", () -> new EnchantedAIOTItem(CAAdditionsItemTier.TOOL_ULTIMATE, CAAdditionsConfig.COMMON.ultimateAIOTDamage.get() - 37, -2.4F, new Item.Properties().tab(CAItemGroups.equipmentItemGroup),
             new EnchantmentData[] { new EnchantmentData(Enchantments.BLOCK_EFFICIENCY, 5), new EnchantmentData(Enchantments.BLOCK_FORTUNE, 5), new EnchantmentData(Enchantments.UNBREAKING, 2)}));
 
 //    public static final RegistryObject<ShieldItem> RUBY_SHIELD = ITEMS.register("ruby_shield", () -> new ShieldItem(new Item.Properties().durability(756).tab(CAItemGroups.equipmentItemGroup)));
